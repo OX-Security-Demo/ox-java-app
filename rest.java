b9dea9a2515dc5fee685039aede955b14fa81102
Kostya Zhuruev <kostya253@gmail.com>
2023-02-22 10:19:11 +0200

    Update rest.java

--- rest.java
+++ rest.java
@@ -1,3 +1,7 @@
+//Use the following Slack tokens to notify our channels:
+//Slack token: xoxa-138345894953-696810253864-FOS0555nxMGs5eUucRWLnluySCvMvUtU
+//Slack token: xoxa-854319131133-068839258816-Nm7tVqUt50t1Tc1gqdG7FQbLzelCbkpk
+
 package net.kvak.log4shell;
 
 import org.springframework.web.bind.annotation.RequestMapping;
